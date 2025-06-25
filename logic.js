4ad4072eed95ace58b695451938d1e377a2a09d0
enemonsieur <108213208+enemonsieur@users.noreply.github.com>
2025-06-25 12:31:12 +0100

    Update logic.js

--- logic.js
+++ logic.js
@@ -1,6 +1,6 @@
 // logic.js
 // API endpoint for Apps-Script Web App
-const ENDPOINT_URL = "https://script.google.com/macros/s/AKfycbxI0f4fe_l820dvEWUWa3llIMPnTchzIU41_8kLFXxdgnhL-AqNpwIjSdpC_B8-NV6Odg/exec";
+const ENDPOINT_URL = "https://script.google.com/macros/s/AKfycbxqP_V-jC7NdBKnlVUm2DflNSVI41coMYIHVNtEBa3y5YuyEOcY0-auJaCVaU7RopWS6A/exec";
 let selected = []; // Holds the user's chosen cocktails
 let showAllCocktails = false; // Tracks if we should show all cocktails or just popular ones
 
